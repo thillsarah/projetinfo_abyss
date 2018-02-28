ab2dac178b5e3be79762b9a12f8894c006efd5a2
Thill Sarah <sthill002@ensc.fr>
2018-02-28 21:48:07 +0100

    yeah

---
+++ TutoRobot/Assets/Scripts/GameControl.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameControl : MonoBehaviour {
+	
+    public static GameControl instance;
+	public GameObject gameOverText;
+	public bool gameOver = false;
+	public Text scoreText;
+
+	private int score = 0;
+
+	public void RobotScored()
+	{
+		if (gameOver)
+		{
+			return;
+		}
+		score++;
+		scoreText.text = "Score: " + score.ToString();
+	}
+
+	public void RobotDied()
+	{
+		gameOverText.SetActive(true);
+		gameOver = true;
+	}
+}
---
+++ TutoRobot/Assets/Scripts/KillZone.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class KillZone : MonoBehaviour {
+
+	public static GameControl instance;
+	public GameObject gameOverText;
+	public bool gameOver = false;
+	public Text scoreText;
+
+    private int score = 0;
+
+    private void OnTriggerEnter2D (Collider2D other) 
+    {
+        if(other.tag == "Player")
+        {
+
+			SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+	}
+
+	public void RobotScored()
+	{
+		if (gameOver)
+		{
+			return;
+		}
+		score++;
+		scoreText.text = "Score: " + score.ToString();
+	}
+
+	public void RobotDied()
+	{
+		gameOverText.SetActive(true);
+		gameOver = true;
+	}
+}
